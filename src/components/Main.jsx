870a9c88bfb88d6338588d17703a43af614222b7
mirkobechini <mirkobechini@gmail.com>
2026-03-19 14:10:25 +0100

    Add function generateAllNewDice to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,26 @@
 import Die from "./Die"
+
+
+function generateAllNewDice() {
+  return new Array(10).fill().map(() => Math.ceil(Math.random() * 6));
+}
+
+
 export default function Main() {
   return (
     <main>
-        <div className="container">
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-            <Die value = {2} />
-        </div>
+      <div className="container">
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+        <Die value={2} />
+      </div>
     </main>
   )
 }
